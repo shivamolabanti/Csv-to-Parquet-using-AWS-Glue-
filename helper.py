483a43d01fd69c96982b26d994da1fb778e6cccb
suddu <sudarshanramp21@gmail.com>
2019-10-03 17:14:24 +0530

    modular

--- helper.py
+++ helper.py
@@ -2,38 +2,35 @@ import boto3
 from botocore.client import ClientError
 import zipfile,os
 import time
-import stack as s
+import utils as u
 s3 = boto3.resource('s3')
 try:
     s3.create_bucket(Bucket='data-suddu', CreateBucketConfiguration={'LocationConstraint': 'ap-south-1'})
 except ClientError:
     print("Data Bucket Already Created")
-s3.Object('data-suddu', 'template.yaml').upload_file(Filename='template.yaml')
+u.upload_object('data-suddu', 'template.yaml', 'template.yaml')
 client = boto3.client('cloudformation')
-try:
-    stack = client.describe_stacks(StackName='week2')
-    if stack['Stacks'][0]['StackStatus'] == 'ROLLBACK_FAILED' or stack['Stacks'][0]['StackStatus'] == 'ROLLBACK_COMPLETE':
-        try:
-            bucket = s3.Bucket('CrawlerTarget')
-            bucket.objects.all().delete()
-        except Exception:
-            print("Bucket Deleted")
-        response = client.delete_stack(StackName='week2')
-        while stack['Stacks'][0]['StackStatus'] == 'DELETE_IN_PROGRESS':
-            try:
-                stack = client.describe_stacks(StackName='stack')
-            except Exception:
-                time.sleep(1)
-                print("Stack Deleted")
-                break
-        s.create_stack()
-        print("Stack Created")
-    else:
-        try:
-            s.update_stack()
-            print("Stack Updated")
-        except ClientError:
-            print("No updates are to be performed")
-except Exception:
-    s.create_stack()
-    print("Stack Created")
+status = u.status_stack('week2')
+if status == 'ROLLBACK_COMPLETE' or status == 'ROLLBACK_FAILED':
+    u.delete_object('CrawlerTarget')
+    client.delete_stack(StackName='week2')
+    while u.status_stack('week2') == 'DELETE_IN_PROGRESS':
+        time.sleep(1)
+    print("stack deleted")
+    u.create_stack('week2', 'https://data-suddu.s3.ap-south-1.amazonaws.com/template.yaml')
+    print("creating stack")
+elif status == 'CREATE_COMPLETE' or status == 'UPDATE_COMPLETE':
+    u.update_stack('week2', 'https://data-suddu.s3.ap-south-1.amazonaws.com/template.yaml')
+    print("updating stack")
+else:
+    u.create_stack('week2', 'https://data-suddu.s3.ap-south-1.amazonaws.com/template.yaml')
+    print("creating stack")
+while u.status_stack('week2') == 'CREATE_IN_PROGRESS' or u.status_stack('week2') == 'UPDATE_IN_PROGRESS' \
+        or u.status_stack('week2') == 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS':
+    time.sleep(1)
+status = u.status_stack('week2')
+print("stack created")
+if status == 'CREATE_COMPLETE' or status == 'UPDATE_COMPLETE':
+    u.upload_object('crawlertarget', 'Sample Data/sample1.csv', 'csv/sample1.csv')
+    u.upload_object('crawlertarget', 'Sample Data/sample2.csv', 'csv/sample2.csv')
+    u.upload_object('crawlertarget', 'Sample Data/sample3.csv', 'csv/sample3.csv')
